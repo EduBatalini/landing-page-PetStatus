30093df49be66b7f3b022cdeac7676656e384141
EduardoBatalini <eduardo_batalini@hotmail.com>
2024-09-17 21:38:12 -0300

    PetStatus

--- aula1/src/pages/Home.tsx
+++ aula1/src/pages/Home.tsx
@@ -5,42 +5,35 @@ import "../styles/buttons.css";
 import "../styles/main.css";
 import "../styles/sobre.css";
 import "../styles/noticias.css";
-
-
-
-
 import HeroRectangleOne from "../assets/img/rectangleOne.png";
 import HeroRectangleTwo from "../assets/img/RectangleTwo.png";
-
-
 import "../styles/hero.css";
-
-
 import Close from '../assets/img/close.svg';
 import Menu from '../assets/img/Menu.svg';
 import Logo from "../assets/img/petsatus.png";
 import Gato from '../assets/img/gato.png'; 
 import Vacina from '../assets/img/vacina.png'
 import Tech from '../assets/img/tech.png'
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/Button";
 
 export default function Home() {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
 
+   
+
     return (
         <>
             <header className="container py-sm">
                 <nav className="flex items-center justify-between">
-                    <img src={Logo} alt="Logo PetStatus" width={200} height={180}  />
+                    <img src={Logo} alt="Logo PetStatus" width={200} height={180} />
                     <div className="desktop-only">
                         <ul className="flex gap-1">
                             <li><a href="#">Home</a></li>
                             <li><a href="#solutions">Soluções</a></li>
                             <li><a href="#testimonials">Depoimentos</a></li>
                             <li><a href="#pricing">Preços</a></li>
-                            <li><a href="#contact">Contato</a></li>
+                            <li><a href="#contact" onClick={(e) => e.preventDefault()}>Contato</a></li>
                         </ul>
                     </div>
                     <div className="desktop-only">
@@ -58,7 +51,7 @@ export default function Home() {
                                         <li><a href="#solutions">Soluções</a></li>
                                         <li><a href="#testimonials">Depoimentos</a></li>
                                         <li><a href="#pricing">Preços</a></li>
-                                        <li><a href="#contact">Contato</a></li>
+                                        <li><a href="#contact" onClick={(e) => e.preventDefault()}>Contato</a></li>
                                         <li><a className="reverse-color" href="#login">Login</a></li>
                                     </ul>
                                     <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
@@ -93,41 +86,35 @@ export default function Home() {
                 </div>
             </section>
 
-
             <div className="new">
-                            
-                                     <h2>Notícias</h2>
-                            <section className="news container py-md">
-                           
-                                <div className="card">
-                                    <img src={Vacina} alt="Vacinação de pets" />
-                                    <div className="card-content">
-                <h3>Vacinação Gratuita para Cães</h3>
-                <p>Participe da nossa campanha de vacinação gratuita para cães durante o mês de setembro. Proteja seu amigo peludo!</p>
-                <a href="#">Leia mais</a>
-                                    </div>
-                                </div>
-                                <div className="card">
-                                <img src={Gato} alt="Vacinação de pets" />
-                                    <div className="card-content">
-                <h3>Cuidados com a Saúde dos Gatos</h3>
-                <p>Confira nossas dicas para manter a saúde dos gatos em dia, desde a alimentação até o check-up regular.</p>
-                <a href="#">Leia mais</a>
-                                    </div>
-                                </div>
-                                <div className="card">
-                                    <img src={Tech} alt="Novo serviço de odontologia" />
-                                    <div className="card-content">
-                <h3>Novo Serviço de Odontologia</h3>
-                <p>Estamos lançando um novo serviço de odontologia para pets. Agende uma consulta e cuide da saúde dental do seu animal!</p>
-                <a href="#">Leia mais</a>
-                                    </div>
-                                </div>
-                            </section>
+                <h2>Notícias</h2>
+                <section className="news container py-md">
+                    <div className="card">
+                        <img src={Vacina} alt="Vacinação de pets" />
+                        <div className="card-content">
+                            <h3>Vacinação Gratuita para Cães</h3>
+                            <p>Participe da nossa campanha de vacinação gratuita para cães durante o mês de setembro. Proteja seu amigo peludo!</p>
+                            <a href="#">Leia mais</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src={Gato} alt="Cuidados com a Saúde dos Gatos" />
+                        <div className="card-content">
+                            <h3>Cuidados com a Saúde dos Gatos</h3>
+                            <p>Confira nossas dicas para manter a saúde dos gatos em dia, desde a alimentação até o check-up regular.</p>
+                            <a href="#">Leia mais</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src={Tech} alt="Novo Serviço de Odontologia" />
+                        <div className="card-content">
+                            <h3>Novo Serviço de Odontologia</h3>
+                            <p>Estamos lançando um novo serviço de odontologia para pets. Agende uma consulta e cuide da saúde dental do seu animal!</p>
+                            <a href="#">Leia mais</a>
+                        </div>
+                    </div>
+                </section>
             </div>
-
-
-
 
             <section id="about" className="container py-md">
                 <h2>Sobre a Clínica</h2>
@@ -147,8 +134,6 @@ export default function Home() {
                     <strong>Endereço:</strong> Av Brasil, 1577, Cascavel, PR
                 </p>
             </section>
-
-
         </>
     );
 }
